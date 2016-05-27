8d5e7c94f5fec594b72cd81105031755223673d3
mayadav <mayadav@visa.com>
2016-05-27 17:48:12 +0530

    modified scripts files to support sample project execution.

--- samples/xml/compileSample.sh
+++ samples/xml/compileSample.sh
@@ -3,10 +3,26 @@
 LOCAL_CP=
 # -----------------------------------------------------------------------------
 # Replace this with cybersource-sdk-java-6.1.0.jar when using Java SDK 1.6 or later.
+# If using this scripts outside zip package then give maven clean install.
+# This will generate all required dependencies under target/dependencies.These dependencies are used in CLASSPATH.
 # -----------------------------------------------------------------------------
-LOCAL_CP=$LOCAL_CP:../../lib/cybersource-sdk-java-6.1.0.jar
 
-if test ! -d ./classes 
+if test -d ../../lib
+then LOCAL_CP=$LOCAL_CP:../../lib/cybersource-sdk-java-6.1.0.jar
+fi
+
+if test ! -d ../../lib
+then
+	if test ! -d ./target
+	then
+		echo "Dependencies are missing."
+		echo "Execute maven clean install , This will generate all required dependencies under target/dependencies!!"
+		exit 1
+	fi
+LOCAL_CP=$LOCAL_CP:target/dependencies/cybersource-sdk-java-6.1.0.jar
+fi
+
+if test ! -d ./classes
 then
    mkdir classes
 fi
@@ -18,3 +34,4 @@ JAVAC_CMD=javac
 fi
 
 $JAVAC_CMD -d ./classes -classpath "$LOCAL_CP" src/main/java/com/cybersource/sample/AuthSample.java
+
