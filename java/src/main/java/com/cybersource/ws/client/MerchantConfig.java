84ba50c44c61845f7253c66d5e7a9351f14769b1
adkumar3 <adkumar@visa.com>
2017-05-30 16:54:32 +0530

    keyfilename parameter change

--- java/src/main/java/com/cybersource/ws/client/MerchantConfig.java
+++ java/src/main/java/com/cybersource/ws/client/MerchantConfig.java
@@ -336,11 +336,13 @@ public class MerchantConfig {
      */
     public File getKeyFile()
     throws ConfigException {
-        File file = new File(
-                             keysDirectory,
-                             keyFilename != null
-                             ? keyFilename : merchantID + ".p12");
-        //File file = new File("/export/home/sunagara/keys/sec_litle.p12");
+        File file;
+    	if (keyFilename == null || keyFilename=="") {
+    		 file = new File(keysDirectory,merchantID + ".p12");
+    	}
+    	else {
+    		file = new File(keysDirectory,keyFilename);
+    	}
         String fullPath = file.getAbsolutePath();
         if (!file.isFile()) {
              throw new ConfigException(
